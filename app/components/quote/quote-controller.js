7737f6db3a5abb5ef7a46c537b75df37460998e6
cody budge <codybudge@gmail.com>
2018-05-11 14:39:13 -0600

    quote works

--- app/components/quote/quote-controller.js
+++ app/components/quote/quote-controller.js
@@ -2,7 +2,20 @@ function QuoteController(){
 
 	var qs = new QuoteService();
 
+	function drawQuote(quote){
+		var template = `
+		<h4>${quote.quote}</h4>
+		<h5>${quote.author}</h5>
+		`;	
+		document.getElementById('quote').innerHTML = template
+	}
+	
+
 	qs.getQuote(function(quote){
-		console.log('What is the quote', quote)
+		qs.getQuote(drawQuote)
+
 	})
+
+
+
 }
--- app/components/weather/weather-controller.js
+++ app/components/weather/weather-controller.js
@@ -1,10 +1,9 @@
 function WeatherController(){
-	var wc = this;
 	var weatherService = new WeatherService();
 
 	function drawWeather(weather){
 		var getTemp = weather.main.temp;
-		var temp =Math.floor(getTemp * 9/5 - 459.67);
+		var temp = Math.floor(getTemp * 9/5 - 459.67);
 		var template =  `
 			<h1>Your temperature is ${temp}F</h1>
 			`
@@ -14,8 +13,7 @@ function WeatherController(){
 	}
 	function getWeather(){
 	weatherService.getWeather(drawWeather)
-		console.log(weather);
-		//What can you do with this weather object?
+	
 	}
 	getWeather()
 }
